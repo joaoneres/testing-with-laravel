1200f398766e98a875d2320355caa911fefff512
João Vitor <leof.neres@gmail.com>
2022-05-13 21:01:18 -0300

    database seeder updates and seeder to hollidays table

--- database/seeders/HollidaysTableSeeder.php
+++ database/seeders/HollidaysTableSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Holliday;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class HollidaysTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            HollidaysTableSeeder::class,
-        ]);
+        Holliday::factory()->times(25)->create();
     }
 }

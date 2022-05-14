e3781d3d4c3e13956b78bc2a479c4699cf532e12
João Vitor <leof.neres@gmail.com>
2022-05-13 21:02:19 -0300

    app service provider updates to consider interface to holliday model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\HollidayRepositoryInterface;
+use App\Repositories\HollidayRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(HollidayRepositoryInterface::class, HollidayRepository::class);
     }
 
     /**

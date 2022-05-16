86e347b3e40b47467fe606e7eaef1675c06c87a7
João Vitor <leof.neres@gmail.com>
2022-05-16 16:51:47 -0300

    AddressRepository

---
+++ app/Repositories/AddressRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\AddressRepositoryInterface;
+use App\Interfaces\CepServiceInterface;
+use App\Models\Address;
+
+class AddressRepository implements AddressRepositoryInterface
+{
+    protected $cep_service;
+
+    public function __construct(CepServiceInterface $cep_service)
+    {
+        $this->cep_service = $cep_service;
+    }
+
+    public function store($cep, $address_number)
+    {
+        $cep_data = $this->cep_service->cep($cep);
+
+        return Address::create([
+            'street' => $cep_data['logradouro'],
+            'cep' => $cep,
+            'number' => $address_number,
+        ]);
+    }
+}
